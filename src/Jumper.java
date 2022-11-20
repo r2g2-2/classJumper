ce013f64a8b860ed88212e37bd2b2faeae34fbbe
r2g2 <r2g2@outlook.co.il>
2022-11-21 00:08:15 +0200

    done

--- src/Jumper.java
+++ src/Jumper.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Jumper {
+public class Jumper {
+    private int id;
+    private int[] allJumps;
+
+    public Jumper(int[] allJumps, int id) {
+        this.allJumps = allJumps;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int[] getAllJumps() {
+        return allJumps;
+    }
+
+    public void setAllJumps(int[] allJumps) {
+        this.allJumps = allJumps;
+    }
+
+    //פעולה שמקבלת מערך של כל הקפיצות ומחזירה את הקפיצה הכי גבוהה
+    static int highestJump(int[] allJumps)
+    {
+        int highestJump=allJumps[0];
+        for (int i = 1; i < allJumps.length; i++)
+        {
+            if (allJumps[i]>= highestJump)
+            {
+                highestJump=allJumps[i];
+            }
+        }
+        return highestJump;
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+
+public class Main
+{
+    /*
+    פעולה חיצונית שמקבלת מערך קופצים ות.ז tz של קופץ ומדפיסה את ת.ז של הקופצים שקפיצתם
+הטובה ביותר גבוהה מזו של הקופץ בעל ת.ז tz .אם לא היה אף אחד כזה תודפס ההודעה"האלוף".
+     */
+    static void gooder(Jumper[] allJumpers, int id)
+    {
+
+        int counter = 0, pass = 0;
+        //לולאה שמוצאת את הקפיצה של הקופץ עם תעודת הזהות
+        for (int i = 0; i < allJumpers.length; i++) {
+            if (allJumpers[i].getId() == id) {
+                pass = Jumper.highestJump(allJumpers[i].getAllJumps());
+            }
+        }
+        for (int i = 0; i < allJumpers.length; i++) {
+            if (Jumper.highestJump(allJumpers[i].getAllJumps()) >= pass) {
+                System.out.println(allJumpers[i].getId());
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            System.out.println("האלוף");
+        }
+
     }
 }
